4af2eb5b642168ebf4c15df1d9d00b1635bcf697
Leander Merbecks <leander.merbecks@gmx.de>
2024-06-03 09:22:54 +0200

    updated group and champion dictionaries

--- em_fun/app.py
+++ em_fun/app.py
@@ -123,20 +123,27 @@ else:
     else:
         st.header('Vote for Group Positions')
         group_dict = {
-            'A': ['Turkey', 'Italy', 'Wales', 'Switzerland'],
-            'B': ['Denmark', 'Finland', 'Belgium', 'Russia'],
-            'C': ['Netherlands', 'Ukraine', 'Austria', 'North Macedonia'],
-            'D': ['England', 'Croatia', 'Scotland', 'Czech Republic'],
-            'E': ['Spain', 'Sweden', 'Poland', 'Slovakia'],
-            'F': ['Hungary', 'Portugal', 'France', 'Germany']
+            "A": ["Germany", "Scotland", "Hungary", "Switzerland"],
+            "B": ["Spain", "Croatia", "Italy", "Albania"],
+            "C": ["Slovenia", "Denmark", "Serbia", "England"],
+            "D": ["Poland", "Netherlands", "Austria", "France"],
+            "E": ["Belgium", "Slovakia", "Romania", "Ukraine"],
+            "F": ["Turkey", "Georgia", "Portugal", "Czech Republic"]
         }
-
+        all_teams = [
+            "Germany", "Scotland", "Hungary", "Switzerland",
+            "Spain", "Croatia", "Italy", "Albania",
+            "Slovenia", "Denmark", "Serbia", "England",
+            "Poland", "Netherlands", "Austria", "France",
+            "Belgium", "Slovakia", "Romania", "Ukraine",
+            "Turkey", "Georgia", "Portugal", "Czech Republic"
+        ]
         # champion, vice champion, third place, fourth place
         champion_dict = {
-            "champion": ["Turkey", "Italy", "Netherlands", "England", "Spain", "Hungary"],
-            "vice_champion": ["Italy", "Turkey", "Ukraine", "Croatia", "Sweden", "Portugal"],
-            "third_place": ["Switzerland", "Wales", "Austria", "Scotland", "Poland", "France"],
-            "fourth_place": ["Wales", "Switzerland", "North Macedonia", "Czech Republic", "Slovakia", "Germany"]
+            "champion": all_teams,
+            "vice_champion": all_teams,
+            "third_place": all_teams,
+            "fourth_place": all_teams
         }
 
         group_positions = {}
