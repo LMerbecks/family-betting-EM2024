8098afab0fdcf31bbcce08bbefa8552afbe7edbb
Constantin Merbecks <merbecks.constantin@bcg.com>
2024-06-14 21:06:19 +0200

    hotfix 2/1

--- em_fun/app.py
+++ em_fun/app.py
@@ -8,59 +8,50 @@ current_directory = os.getcwd()
 st.title('EM 2024 Tippspiel')
 # file_path = 
 
-st.markdown(current_directory)
 
-# List all files and directories in the current working directory
-files_and_directories = os.listdir(current_directory)
+groups = pd.read_parquet(os.path.join(current_directory, "em_fun/group_votes.parquet"))
+groups.columns = groups.columns.get_level_values(1)
+groups["Punkte"] = 2
 
-# Display the list
-st.markdown("**Contents of the current working directory:**")
-for item in files_and_directories:
-    st.markdown(f"- {item}")
+ranks = pd.read_parquet(os.path.join(current_directory, "em_fun/overall_votes.parquet"))
 
-# groups = pd.read_parquet(os.path.join(current_directory, "group_votes.parquet"))
-# groups.columns = groups.columns.get_level_values(1)
-# groups["Punkte"] = 2
+st.markdown("## Gruppen-Tipps")
+st.table(groups) 
 
-# ranks = pd.read_parquet(os.path.join(current_directory, "overall_votes.parquet"))
+st.markdown("## Europameister Tipps")
+st.table(ranks.set_index("Tipp")) 
 
-# st.markdown("## Gruppen-Tipps")
-# st.table(groups) 
-
-# st.markdown("## Europameister Tipps")
-# st.table(ranks.set_index("Tipp")) 
-
-# # Plot a bar chart with the leading person
+# Plot a bar chart with the leading person
 
 
-# st.markdown("## Live Tippstand")
+st.markdown("## Live Tippstand")
 
-# # Sample data
-# data = {
-#     'Names': ['Augi', 'Consi', 'Lele', 'Lia & Klaus', 'Mama', 'Oma', 'Papa'],
-#     'Points': [0, 1, 0, 0, 0, 0, 0]
-# }
+# Sample data
+data = {
+    'Names': ['Augi', 'Consi', 'Lele', 'Lia & Klaus', 'Mama', 'Oma', 'Papa'],
+    'Points': [0, 1, 0, 0, 0, 0, 0]
+}
 
-# df = pd.DataFrame(data)
+df = pd.DataFrame(data)
 
-# # Create bar chart with different colors for each bar
-# colors = ['skyblue', 'lightgreen', 'lightcoral', 'lightsalmon', 'lightpink', 'lightseagreen', 'lightsteelblue']
-# plt.style.use('dark_background')
+# Create bar chart with different colors for each bar
+colors = ['skyblue', 'lightgreen', 'lightcoral', 'lightsalmon', 'lightpink', 'lightseagreen', 'lightsteelblue']
+plt.style.use('dark_background')
 
-# # Create bar chart with different colors for each bar and additional horizontal lines
-# plt.figure(figsize=(10, 6))
-# plt.bar(df['Names'], df['Points'], color=colors)
-# plt.axhline(y=5, color='blue', linestyle='--', label='Anfänger')
-# plt.axhline(y=12, color='red', linestyle='--', label='Heimlicher Tipico Wettstudio Besucher')
-# # plt.axhline(y=17, color='green', linestyle='--', label='Profi Tipper')
+# Create bar chart with different colors for each bar and additional horizontal lines
+plt.figure(figsize=(10, 6))
+plt.bar(df['Names'], df['Points'], color=colors)
+plt.axhline(y=5, color='blue', linestyle='--', label='Anfänger')
+plt.axhline(y=12, color='red', linestyle='--', label='Heimlicher Tipico Wettstudio Besucher')
+# plt.axhline(y=17, color='green', linestyle='--', label='Profi Tipper')
 
-# plt.xlabel('Tipper')
-# plt.ylabel('Punkte')
-# plt.title('Live Tippstand')
-# plt.xticks(rotation=45)
-# plt.legend()
-# plt.tight_layout()
+plt.xlabel('Tipper')
+plt.ylabel('Punkte')
+plt.title('Live Tippstand')
+plt.xticks(rotation=45)
+plt.legend()
+plt.tight_layout()
 
-# # Show plot
-# st.pyplot(plt)
+# Show plot
+st.pyplot(plt)
 
